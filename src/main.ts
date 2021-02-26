c01e3521cef7cfbdd68d45c5b35c22267cdd7348
Gabriel <gabi.acosta.engler@gmail.com>
2021-02-26 15:56:28 +0100

    add filename to command.

--- src/main.ts
+++ src/main.ts
@@ -126,7 +126,7 @@ async function trySign(file: string): Promise<boolean> {
 		await wait(i);
 		if (supportedFileExt.includes(ext)) {
 			try {
-				const command = `"${signtool}" sign /sm /t ${timestmpServer} /sha1 "${sha1}"`;
+				const command = `"${signtool}" sign /sm /t ${timestmpServer} /sha1 "${sha1}" /f ${file}`;
 				if (certDesc !== '') command.concat(` /d ${certDesc}`);
 
 				command.concat(` ${file}`);
